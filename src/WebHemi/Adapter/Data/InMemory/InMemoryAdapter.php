ccd64287bca695e880a77fa8c929cc349988c6cf
Gixx <gixx@gixx-web.com>
2016-06-24 12:55:23 +0200

    Add InMemoryAdapter + unit test; add PDOAdapter unit test.

--- src/WebHemi/Adapter/Data/InMemory/InMemoryAdapter.php
+++ src/WebHemi/Adapter/Data/InMemory/InMemoryAdapter.php
@@ -12,33 +12,54 @@
 namespace WebHemi\Adapter\Data\InMemory;
 
 use WebHemi\Adapter\Data\DataAdapterInterface;
+use WebHemi\Adapter\Exception\InitException;
+use WebHemi\Adapter\Exception\InvalidArgumentException;
 
 /**
  * Class InMemoryAdapter.
  */
 class InMemoryAdapter implements DataAdapterInterface
 {
-    /** @var mixed */
+    const EXPRESSION_IN_ARRAY = 'in';
+    const EXPRESSION_WILDCARD = 'like';
+
+    /** @var array */
     private $dataStorage;
     /** @var string */
-    private $dataGroup = null;
+    private $dataGroup = 'default';
     /** @var string */
-    private $idKey = null;
+    private $idKey = 'id';
 
     /**
      * PDOAdapter constructor.
      *
      * @param mixed $dataStorage
+     *
+     * @throws InvalidArgumentException
      */
     public function __construct($dataStorage = null)
     {
-        $this->dataStorage = $dataStorage;
+        if (empty($dataStorage)) {
+            $dataStorage = [];
+        }
+
+        if (!is_array($dataStorage)) {
+            throw new InvalidArgumentException('The constructor parameter must be empty or an array.');
+        }
+
+        foreach ($dataStorage as $rowData) {
+            if (!is_array($rowData)) {
+                throw new InvalidArgumentException('The constructor parameter if present must be an array of arrays.');
+            }
+        }
+
+        $this->dataStorage[$this->dataGroup] = $dataStorage;
     }
 
     /**
      * Returns the Data Storage instance.
      *
-     * @return mixed
+     * @return array
      */
     public function getDataStorage()
     {
@@ -50,11 +71,22 @@ class InMemoryAdapter implements DataAdapterInterface
      *
      * @param string $dataGroup
      *
+     * @throws InitException
+     *
      * @return InMemoryAdapter
      */
     public function setDataGroup($dataGroup)
     {
+        // Allow to change only once.
+        if ($this->dataGroup !== 'default') {
+            throw new InitException('Can\'t re-initialize dataGroup property. Property is already set.');
+        }
+
         $this->dataGroup = $dataGroup;
+
+        // Copy all previous init data.
+        $this->dataStorage[$dataGroup] = $this->dataStorage['default'];
+        unset($this->dataStorage['default']);
 
         return $this;
     }
@@ -64,10 +96,17 @@ class InMemoryAdapter implements DataAdapterInterface
      *
      * @param string $idKey
      *
-     * @return InMemoryAdapter
+     * @throws InitException
+     *
+     * @return $this
      */
     public function setIdKey($idKey)
     {
+        // Allow to change only once.
+        if ($this->idKey !== 'id') {
+            throw new InitException('Can\'t re-initialize idKey property. Property is already set.');
+        }
+
         $this->idKey = $idKey;
 
         return $this;
@@ -82,9 +121,13 @@ class InMemoryAdapter implements DataAdapterInterface
      */
     public function getData($identifier)
     {
-        $result = [$identifier];
+        $result = [];
 
-        // todo implement SELECT query
+        $dataStorage = $this->getDataStorage()[$this->dataGroup];
+
+        if (isset($dataStorage[$identifier])) {
+            $result = $dataStorage[$identifier];
+        }
 
         return $result;
     }
@@ -100,12 +143,117 @@ class InMemoryAdapter implements DataAdapterInterface
      */
     public function getDataSet(array $expression, $limit = null, $offset = null)
     {
-        $entityList = [$expression, $limit, $offset];
+        $result = [];
 
-        // todo implement SELECT query
+        $dataStorage = $this->getDataStorage()[$this->dataGroup];
+        $limitCounter = 0;
+        $offsetCounter = 0;
 
-        return $entityList;
+        foreach ($dataStorage as $data) {
+            if ($this->isExpressionMatch($expression, $data)) {
+                if (!is_null($limit) && $limitCounter >= $limit) {
+                    break;
+                }
+
+                if (!is_null($offset) && $offsetCounter++ < $offset) {
+                    continue;
+                }
+
+                $limitCounter++;
+                $result[] = $data;
+            }
+        }
+
+        return $result;
     }
+
+    /**
+     * Checks the data (row) array against the expressions.
+     *
+     * @param array $expression
+     * @param array $data
+     *
+     * @return bool
+     */
+    private function isExpressionMatch(array $expression, array $data)
+    {
+        $match = true;
+
+        foreach ($expression as $pattern => $subject) {
+            $dataKey = '';
+
+            switch ($this->getExpressionType($pattern, $dataKey)) {
+                case self::EXPRESSION_WILDCARD:
+                    $subject = str_replace('%', '.*', $subject);
+                    $match = preg_match('/^' . $subject . '$/', $data[$dataKey]);
+                    break;
+
+                case self::EXPRESSION_IN_ARRAY:
+                    $match = in_array($data[$dataKey], (array)$subject);
+                    break;
+
+                case '<':
+                    $match = $data[$dataKey] < $subject;
+                    break;
+
+                case '<=':
+                    $match = $data[$dataKey] <= $subject;
+                    break;
+
+                case '>':
+                    $match = $data[$dataKey] > $subject;
+                    break;
+
+                case '>=':
+                    $match = $data[$dataKey] >= $subject;
+                    break;
+
+                case '<>':
+                    $match = $data[$dataKey] != $subject;
+                    break;
+
+                default:
+                    $match = $data[$dataKey] == $subject;
+            }
+
+            // First false means some expression is failing for the data row, so the whole expression set is failing.
+            if (!$match) {
+                break;
+            }
+        }
+
+        return (bool)$match;
+    }
+
+    /**
+     * Gets expression type and also sets the expression subject.
+     *
+     * @param string $pattern
+     * @param string $subject
+     *
+     * @return string
+     */
+    private function getExpressionType($pattern, &$subject)
+    {
+        $type = '=';
+        $subject = $pattern;
+        $matches = [];
+
+        if (preg_match('/^(?P<subject>[^\s]+)\s+(?P<relation>(\<\>|\<=|\>=|=|\<|\>))\s+\?$/', $pattern, $matches)) {
+            $type = $matches['relation'];
+            $subject = $matches['subject'];
+        } elseif (preg_match('/^(?P<subject>[^\s]+)\s+(?P<relation>LIKE)\s+\?$/', $pattern, $matches)) {
+            $type = self::EXPRESSION_WILDCARD;
+            $subject = $matches['subject'];
+        } elseif (preg_match('/^(?P<subject>[^\s]+)\s+(?P<relation>IN)\s?\(?\?\)?$/', $pattern, $matches)) {
+            $type = self::EXPRESSION_IN_ARRAY;
+            $subject = $matches['subject'];
+        }
+
+        return $type;
+    }
+
+
 
     /**
      * Get the number of matched data in the set according to the expression.
@@ -131,11 +279,27 @@ class InMemoryAdapter implements DataAdapterInterface
      */
     public function saveData($identifier, array $data)
     {
-        $returnId = [$identifier, $data];
+        $dataStorage = $this->getDataStorage()[$this->dataGroup];
 
-        // todo implement INSERT / UPDATE queries
+        if (empty($dataStorage) && empty($identifier)) {
+            $identifier = 1;
+        }
 
-        return $returnId;
+        if (empty($identifier)) {
+            $keys = array_keys($dataStorage);
+            $maxKey = array_pop($keys);
+
+            if (is_numeric($maxKey)) {
+                $identifier = (int) $maxKey + 1;
+            } else {
+                $identifier = $maxKey . '_1';
+            }
+        }
+
+        // To make it sure, we always apply changes on the exact property.
+        $this->dataStorage[$this->dataGroup][$identifier] = $data;
+
+        return $identifier;
     }
 
     /**
@@ -147,9 +311,12 @@ class InMemoryAdapter implements DataAdapterInterface
      */
     public function deleteData($identifier)
     {
-        $result = (bool) $identifier;
-
-        // todo implement DELETE query
+        $result = false;
+        $dataFound = $this->getData($identifier);
+        if (!empty($dataFound)) {
+            unset($this->dataStorage[$this->dataGroup][$identifier]);
+            $result = true;
+        }
 
         return $result;
     }
