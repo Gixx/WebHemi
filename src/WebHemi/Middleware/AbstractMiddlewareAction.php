12ea5bb1a5f58d7627781bb7fc63a6bb0fca4912
Gixx <gixx@gixx-web.com>
2016-09-09 15:54:38 +0200

    Small fixes, nothing serious ;)

--- src/WebHemi/Middleware/AbstractMiddlewareAction.php
+++ src/WebHemi/Middleware/AbstractMiddlewareAction.php
@@ -13,10 +13,6 @@ namespace WebHemi\Middleware;
 
 use WebHemi\Adapter\Http\ResponseInterface;
 use WebHemi\Adapter\Http\ServerRequestInterface;
-use WebHemi\DataEntity\User\UserEntity;
-use WebHemi\DataStorage\User\UserStorage;
-use WebHemi\Middleware\MiddlewareInterface;
-use WebHemi\Middleware\MiddlewareActionInterface;
 
 /**
  * Class AbstractMiddlewareAction.
