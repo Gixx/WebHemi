8b83a2cee519205404bf9e667e8a9c1c387e9ac6
Gabor Ivan <gabor.ivan@westwing.de>
2017-01-05 16:08:35 +0100

    Update copyright year

--- src/WebHemi/Adapter/Router/FastRoute/FastRouteAdapter.php
+++ src/WebHemi/Adapter/Router/FastRoute/FastRouteAdapter.php
@@ -4,7 +4,7 @@
  *
  * PHP version 5.6
  *
- * @copyright 2012 - 2016 Gixx-web (http://www.gixx-web.com)
+ * @copyright 2012 - 2017 Gixx-web (http://www.gixx-web.com)
  * @license   https://opensource.org/licenses/MIT The MIT License (MIT)
  * @link      http://www.gixx-web.com
  */
