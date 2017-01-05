497387196011525929b2da9f3b8b1124528b82ac
Gabor Ivan <gabor.ivan@westwing.de>
2017-01-05 16:20:24 +0100

    Fist steps to PHP 7

--- resources/default_theme/static/js/site.js
+++ resources/default_theme/static/js/site.js
@@ -1,8 +1,6 @@
 /**
  * WebHemi
  *
- * PHP version 5.6
- *
  * @copyright 2012 - 2017 Gixx-web (http://www.gixx-web.com)
  * @license   https://opensource.org/licenses/MIT The MIT License (MIT)
  * @link      http://www.gixx-web.com
