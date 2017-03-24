56272f0bb012b31f96df70f2ea46b56af52bd276
Gabor Ivan <gabor.ivan@westwing.de>
2017-03-24 16:44:12 +0100

    Throw exception in router instead of response

--- src/WebHemi/Http/ResponseInterface.php
+++ src/WebHemi/Http/ResponseInterface.php
@@ -25,6 +25,7 @@ interface ResponseInterface extends PsrResponseInterface
     public const STATUS_REDIRECT = 302;
     public const STATUS_UNAUTHORIZED = 401;
     public const STATUS_FORBIDDEN = 403;
+    public const STATUS_NOT_FOUND = 404;
     public const STATUS_BAD_METHOD = 405;
     public const STATUS_INTERNAL_SERVER_ERROR = 500;
 }
