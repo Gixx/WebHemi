f3c035e9bf8bf6f1cbdc0ed91661dd825c204fb7
Gabor Ivan <gabor.ivan@westwing.de>
2017-04-10 11:23:14 +0200

    Add empty actions for Control Panel items which will throw 501 Not Implemented.

---
+++ src/WebHemi/Middleware/Action/Admin/ControlPanel/Policies/IndexAction.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * WebHemi.
+ *
+ * PHP version 7.1
+ *
+ * @copyright 2012 - 2017 Gixx-web (http://www.gixx-web.com)
+ * @license   https://opensource.org/licenses/MIT The MIT License (MIT)
+ *
+ * @link      http://www.gixx-web.com
+ */
+declare(strict_types = 1);
+
+namespace WebHemi\Middleware\Action\Admin\ControlPanel\Policies;
+
+use RuntimeException;
+use WebHemi\Middleware\Action\AbstractMiddlewareAction;
+
+/**
+ * Class IndexAction
+ */
+class IndexAction extends AbstractMiddlewareAction
+{
+    /**
+     * Gets template map name or template file path.
+     *
+     * @return string
+     */
+    public function getTemplateName() : string
+    {
+        return 'admin-control-panel-policies-list';
+    }
+
+    /**
+     * Gets template data.
+     *
+     * @return array
+     */
+    public function getTemplateData() : array
+    {
+        throw new RuntimeException('Not Implemented', 501);
+        return [];
+    }
+}
