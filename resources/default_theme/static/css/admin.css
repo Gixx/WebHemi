b341d3bb27d06dd6fc2a7cd79b82b9c6fe9cf8f2
Gabor Ivan <gabor.ivan@westwing.de>
2017-06-27 14:52:26 +0200

    Start designing the default theme

---
+++ resources/default_theme/static/css/admin.css
@@ -0,0 +1,7 @@
+/**
+ * WebHemi
+ *
+ * @copyright 2012 - 2017 Gixx-web (http://www.gixx-web.com)
+ * @license   https://opensource.org/licenses/MIT The MIT License (MIT)
+ * @link      http://www.gixx-web.com
+ */
