0cbf53329b94cf48ffb41acccca3a17f81f99507
Gixx <gixx@gixx-web.com>
2018-01-03 10:23:15 +0100

    Change copyright date to 2018

--- src/WebHemi/Router/ServiceAdapter/Base/ServiceAdapter.php
+++ src/WebHemi/Router/ServiceAdapter/Base/ServiceAdapter.php
@@ -4,7 +4,7 @@
  *
  * PHP version 7.1
  *
- * @copyright 2012 - 2017 Gixx-web (http://www.gixx-web.com)
+ * @copyright 2012 - 2018 Gixx-web (http://www.gixx-web.com)
  * @license   https://opensource.org/licenses/MIT The MIT License (MIT)
  * @link      http://www.gixx-web.com
  */
