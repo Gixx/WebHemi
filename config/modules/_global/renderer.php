f00b7f542d19c7c1e9d4112bd9bca9ef04ffcffe
Gixx <gixx@gixx-web.com>
2018-12-19 13:25:30 +0100

    Add missing copyright year changes

--- config/modules/_global/renderer.php
+++ config/modules/_global/renderer.php
@@ -4,7 +4,7 @@
  *
  * PHP version 7.2
  *
- * @copyright 2012 - 2018 Gixx-web (http://www.gixx-web.com)
+ * @copyright2012 - 2019 Gixx-web (http://www.gixx-web.com)
  * @license   https://opensource.org/licenses/MIT The MIT License (MIT)
  *
  * @link      http://www.gixx-web.com
