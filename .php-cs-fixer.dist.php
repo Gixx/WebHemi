434ebaa75822fee851964400bc120e750cad372b
Gabor Ivan <navig80@gmail.com>
2026-05-02 16:53:47 +0200

    chore(qa): align fixer/linter/analyzer pipeline and deptrac rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -11,7 +11,7 @@ $finder = (new PhpCsFixer\Finder())
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@Symfony' => true,
+        '@PSR12' => true,
     ])
     ->setFinder($finder)
 ;
