4885a8f8a7a5cd96ca1447184f7bf9c7eccee6bb
Gabor Ivan <navig80@gmail.com>
2026-05-02 20:06:06 +0200

    feat(permission): add admin CRUD

---
+++ src/Admin/Controller/PermissionController.php
@@ -0,0 +1,98 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Admin\Controller;
+
+use App\Entity\Permission;
+use App\Repository\PermissionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/admin/permissions', name: 'admin_permission_')]
+final class PermissionController extends AbstractController
+{
+    public function __construct(
+        private readonly PermissionRepository $permissionRepository,
+        private readonly EntityManagerInterface $entityManager,
+    ) {
+    }
+
+    #[Route(name: 'list', methods: ['GET'])]
+    public function list(): Response
+    {
+        return $this->render('admin/permission/list.html.twig', [
+            'permissions' => $this->permissionRepository->findBy([], ['name' => 'ASC']),
+        ]);
+    }
+
+    #[Route('/create', name: 'create', methods: ['GET', 'POST'])]
+    public function create(Request $request): Response
+    {
+        $permission = new Permission();
+
+        if ($request->isMethod(Request::METHOD_POST)) {
+            $permission
+                ->setName($request->request->getString('name', ''))
+                ->setLabel($request->request->getString('label', ''));
+
+            if ('' !== $permission->getName() && '' !== $permission->getLabel()) {
+                $this->entityManager->persist($permission);
+                $this->entityManager->flush();
+                $this->addFlash('success', 'Permission created successfully.');
+
+                return $this->redirectToRoute('admin_permission_list');
+            }
+
+            $this->addFlash('error', 'Name and label are required.');
+        }
+
+        return $this->render('admin/permission/form.html.twig', [
+            'permission' => $permission,
+            'title' => 'Create Permission',
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(int $id, Request $request): Response
+    {
+        $permission = $this->permissionRepository->find($id);
+        if (null === $permission) {
+            throw $this->createNotFoundException('Permission not found.');
+        }
+
+        if ($request->isMethod(Request::METHOD_POST)) {
+            $permission
+                ->setName($request->request->getString('name', ''))
+                ->setLabel($request->request->getString('label', ''));
+
+            $this->entityManager->flush();
+            $this->addFlash('success', 'Permission updated successfully.');
+
+            return $this->redirectToRoute('admin_permission_list');
+        }
+
+        return $this->render('admin/permission/form.html.twig', [
+            'permission' => $permission,
+            'title' => 'Edit Permission',
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'delete', methods: ['POST'])]
+    public function delete(int $id): Response
+    {
+        $permission = $this->permissionRepository->find($id);
+        if (null === $permission) {
+            throw $this->createNotFoundException('Permission not found.');
+        }
+
+        $this->entityManager->remove($permission);
+        $this->entityManager->flush();
+        $this->addFlash('success', 'Permission deleted successfully.');
+
+        return $this->redirectToRoute('admin_permission_list');
+    }
+}
